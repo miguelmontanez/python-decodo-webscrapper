844c6a93f307b75f213f62281d698d9638760a76
miguelmontanez <miguelmontanez.busines@gmail.com>
2025-07-19 10:02:54 +0200

    simplify_scrapper

--- main.py
+++ main.py
@@ -12,40 +12,34 @@ mcp = FastMCP("DecodoWebsiteScrapper")
 
 # Tool: Get Article Text
 @mcp.tool()
-def get_article_text(website_url:str, div_id: str) -> str:
-    """Scrapes a website HTML and extracts the text from the given div Id"""
-
-    # Scrape Website
+def get_article_text(website_url: str) -> str:
+    """Scapres a wesbite HTML and extracts the text from it """
+    
     url = "https://scraper-api.decodo.com/v2/scrape"
-  
+    
     payload = {
-        "url": website_url
+        "url": website_url,
+        "headless": "html"
     }
+    
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "authorization": "Basic " + decodo_token
+        "authorization": "Basic "+decodo_token
     }
+    
     crawled_article = requests.post(url, json=payload, headers=headers)
     crawled_article_json = json.loads(crawled_article.text)
 
-    # If article is not crawled correctly retutn None
-    status_code = crawled_article_json['results'][0]['status_code']
-    if status_code!=200:
-        return "Website Can't be Crawled"
+    status_code = crawled_article_json['results'][0]["status_code"]
+    if status_code !=200:
+        return "Website Can't be crawled"
     
-    # Create BeautifulSoup object from HTML
-    html_string = crawled_article_json['results'][0]['content']
-    soup = BeautifulSoup(html_string,'html.parser')
-
-    # Get Article Text
-    story_div = soup.find('div',id=div_id)
-    if story_div is None:
-        return "Div does not exist"
+    html_string = crawled_article_json['results'][0]["content"]
     
-    text = story_div.get_text(strip=True,separator='\n')
+    return BeautifulSoup(html_string, "html.parser").get_text()
 
-    return text
+
 
 if __name__ == "__main__":
     mcp.run()
